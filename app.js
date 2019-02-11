7508d14ba787a0bbeb18fbfbc37b251286e0eedf
Tae-yoon <yeotaeyoon@gmail.com>
2019-02-11 16:19:02 +0900

    add Header and Footer partials, include Semantic UI and simple nav

--- app.js
+++ app.js
@@ -11,6 +11,33 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTFUL ROUTES
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err) console.log(err);
+
+        res.render("index", {blogs});
+    })
+    
+});
+
 
 app.listen(3000, (err) => {
     if (err) console.log(err);
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Blog Index
+* Setup the Blog App
+* Create the Blog model
+* Add INDEX route and template
+
+# Basic Layout
+* Add Header and Footer Partials
+* Include Semantic UI
+* Add Simple Nav
